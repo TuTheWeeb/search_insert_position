6dfb650eac32f2f30a0c97d970e91f55f8a641e9
TuTheWeeb <eduardo.tuler@gmail.com>
2023-02-20 00:21:52 +0000

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+
+// The function tha will run the code
+fn search_insert_position (arr: &[i32;5], target: &i32) -> usize {
+    
+    // Declaring the return variable and the lenght of the array
+    let mut _result: usize = 0;
+    let arr_len: usize = arr.len();
+
+    // For loop, right now is O(n)
+    for x in 0..arr_len {
+        
+        // Check to see if the target is already in the array
+        if arr[x] == *target {
+        
+            _result = x;
+            break;
+        } 
+        
+        // If it's not in the array, then check to see where it should be
+        else if arr[x] > *target {
+            
+            // Check to see if the index is 0
+            if arr[x] == 0 {
+                _result = 0;
+            }
+
+            // If it's not then put _result as x 
+            else {
+                _result = x;
+            }
+
+            break;
+        }
+    }
+
+    return _result;
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    // Declare the array and the target   
+    let arr: [i32; 5] = [1,3,4,5,6];
+    let target: i32 = 2;
+
+    // Print the result
+    println!("{}", search_insert_position(&arr, &target));
 }
